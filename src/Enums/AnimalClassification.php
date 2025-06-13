a9b13b44f9418a623450e9f1e09bb7f17ae44f71
denis fedulov <den.fedulov.1999@mail.ru>
2025-06-13 12:26:39 +0300

    Create AnimalClassification.php

---
+++ src/Enums/AnimalClassification.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dfed\PeshkarikiTest\Enums;
+
+abstract class AnimalClassification
+{
+    const MAMMAL = 'mammal';
+    const BIRD = 'bird';
+    const REPTILE = 'reptile';
+    const INSECT = 'insect';
+    const ARACHNID = 'arachnid';
+}

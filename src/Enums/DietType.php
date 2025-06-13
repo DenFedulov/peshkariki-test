2dc6e6d2a7b88d832440aa39a4d73dc61f16556f
denis fedulov <den.fedulov.1999@mail.ru>
2025-06-13 12:26:41 +0300

    Create DietType.php

---
+++ src/Enums/DietType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Dfed\PeshkarikiTest\Enums;
+
+abstract class DietType
+{
+    const OMNIVORE = 'omnivore';
+    const HERBIVORE = 'herbivore';
+    const CARNIVORE = 'carnivore';
+}

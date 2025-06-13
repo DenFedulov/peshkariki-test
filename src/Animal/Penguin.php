51e0d9a4cfcb2f1242ec685b7b477b539eac9e76
denis fedulov <den.fedulov.1999@mail.ru>
2025-06-13 12:30:07 +0300

    Create Penguin.php

---
+++ src/Animal/Penguin.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Dfed\PeshkarikiTest\Animal;
+
+use Dfed\PeshkarikiTest\Animal\Classification\Bird;
+use Dfed\PeshkarikiTest\Animal\Strategy\FemaleOviReproductionStrategy;
+use Dfed\PeshkarikiTest\Animal\Strategy\MaleOviReproductionStrategy;
+use Dfed\PeshkarikiTest\Enums\DietType;
+use Dfed\PeshkarikiTest\Enums\SexType;
+
+class Penguin extends Bird
+{
+    /**
+     * @param  AnimalOptions $options
+     * @return void
+     */
+    public function __construct(AnimalOptions $options)
+    {
+        parent::__construct($options, new AnimalSpecs(
+            parent::CLASSIFICATION,
+            DietType::CARNIVORE
+        ));
+        switch($options->getSexType()){
+            case SexType::MALE:
+                $this->reproductionStrategy = new MaleOviReproductionStrategy();
+                break;
+            case SexType::FEMALE:
+                $this->reproductionStrategy = new FemaleOviReproductionStrategy();
+                break;
+            default:
+                throw new \InvalidArgumentException('Inknown sex type for ' . self::class . ': ' . $options->getSexType());  
+                break;
+        }
+    }
+
+    public function advanceAge(): void
+    {
+        // TODO
+    }
+
+    public function decreaseSatiation(): void
+    {
+        // TODO
+    }
+
+    public function eat(): void
+    {
+        // TODO
+    }
+
+    public function move(): void
+    {
+        // TODO
+    }
+}

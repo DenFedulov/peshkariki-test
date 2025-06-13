c8128e835cb06c31478c5c9ff6cd30b7397bcdcc
denis fedulov <den.fedulov.1999@mail.ru>
2025-06-13 12:30:06 +0300

    Create Parrot.php

---
+++ src/Animal/Parrot.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Dfed\PeshkarikiTest\Animal;
+
+use Dfed\PeshkarikiTest\Animal\Behavior\Flying;
+use Dfed\PeshkarikiTest\Animal\Classification\Bird;
+use Dfed\PeshkarikiTest\Animal\Strategy\FemaleOviReproductionStrategy;
+use Dfed\PeshkarikiTest\Animal\Strategy\MaleOviReproductionStrategy;
+use Dfed\PeshkarikiTest\Animal\Trait\Flight;
+use Dfed\PeshkarikiTest\Enums\DietType;
+use Dfed\PeshkarikiTest\Enums\SexType;
+
+class Parrot extends Bird implements Flying
+{
+    /**
+     * @param  AnimalOptions $options
+     * @return void
+     */
+    public function __construct(AnimalOptions $options)
+    {
+        parent::__construct($options, new AnimalSpecs(
+            parent::CLASSIFICATION,
+            DietType::HERBIVORE
+        ));
+        switch($options->getSexType()){
+            case SexType::MALE:
+                $this->reproductionStrategy = new MaleOviReproductionStrategy();
+                break;
+            case SexType::FEMALE:
+                $this->reproductionStrategy = new FemaleOviReproductionStrategy();
+                break;
+            default:
+                throw new \InvalidArgumentException('Inknown sex type for ' . self::class . ': ' . $options->getSexType());  
+                break;
+        }
+    }
+
+    public function advanceAge(): void
+    {
+        // TODO
+    }
+
+    public function decreaseSatiation(): void
+    {
+        // TODO
+    }
+
+    public function eat(): void
+    {
+        // TODO
+    }
+
+    public function move(): void
+    {
+        // TODO
+    }
+
+    use Flight;
+}

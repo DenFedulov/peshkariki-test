fb52265cd98c300b3baff084d44f3fa4607a9cfa
denis fedulov <den.fedulov.1999@mail.ru>
2025-06-13 12:30:12 +0300

    Create Wolf.php

---
+++ src/Animal/Wolf.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Dfed\PeshkarikiTest\Animal;
+
+use Dfed\PeshkarikiTest\Animal\Classification\Mammal;
+use Dfed\PeshkarikiTest\Animal\Strategy\FemaleViviReproductionStrategy;
+use Dfed\PeshkarikiTest\Animal\Strategy\MaleViviReproductionStrategy;
+use Dfed\PeshkarikiTest\Enums\DietType;
+use Dfed\PeshkarikiTest\Enums\SexType;
+
+class Wolf extends Mammal
+{
+    /**
+     * @param  AnimalOptions $options
+     * @return void
+     */
+    public function __construct(AnimalOptions $options)
+    {
+        parent::__construct($options, new AnimalSpecs(
+            parent::CLASSIFICATION,
+            DietType::CARNIVORE
+        ));
+        switch($options->getSexType()){
+            case SexType::MALE:
+                $this->reproductionStrategy = new MaleViviReproductionStrategy();
+                break;
+            case SexType::FEMALE:
+                $this->reproductionStrategy = new FemaleViviReproductionStrategy();
+                break;
+            default:
+                throw new \InvalidArgumentException('Inknown sex type for ' . self::class . ': ' . $options->getSexType());  
+                break;
+        }
+    }
+
+    public function advanceAge(): void
+    {
+        // TODO
+    }
+
+    public function decreaseSatiation(): void
+    {
+        // TODO
+    }
+
+    public function eat(): void
+    {
+        // TODO
+    }
+
+    public function move(): void
+    {
+        // TODO
+    }
+}
